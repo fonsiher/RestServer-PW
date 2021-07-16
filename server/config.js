7499e3ef59898697bdd93ca912cd545d5531ddd8
fonsiher <ehernandezm@est.ups.edu.ec>
2021-07-16 11:49:22 -0500

    variable base vinculada

--- server/config.js
+++ server/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/Cafe'
 } else {
-    urlDB = 'mongodb+srv://fonsiher:8yaGsCCmg1BDMpJo@sully.aciue.mongodb.net/Cafe?retryWrites=true&w=majority'
+    urlDB = process.env.BDD_URL
 }
 
 process.env.URLDB = urlDB;
